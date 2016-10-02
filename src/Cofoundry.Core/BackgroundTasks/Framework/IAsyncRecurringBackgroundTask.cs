c037d54eecf91666d94f6623ea188a925daf9ebb
Joel Mitchell <me@joelmmitchell.co.uk>
2016-10-02 13:10:05 +0100

    Added async versions of the background tasks api

---
+++ src/Cofoundry.Core/BackgroundTasks/Framework/IAsyncRecurringBackgroundTask.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cofoundry.Core.BackgroundTasks
+{
+    /// <summary>
+    /// Represents a task to execute every time a time interval expires.
+    /// </summary>
+    public interface IAsyncRecurringBackgroundTask : IAsyncBackgroundTask
+    {
+    }
+}
--- src/Cofoundry.Core/BackgroundTasks/Framework/IBackgroundTaskScheduler.cs
+++ src/Cofoundry.Core/BackgroundTasks/Framework/IBackgroundTaskScheduler.cs
@@ -49,5 +49,39 @@ namespace Cofoundry.Core.BackgroundTasks
         /// <typeparam name="TTask">The type of task to execute</typeparam>
         /// <returns>Instance of IBackgroundTaskScheduler for method chaining</returns>
         IBackgroundTaskScheduler DeregisterRecurringTask<TTask>() where TTask : IRecurringBackgroundTask;
+
+        /// <summary>
+        /// Register a job to run every X number of days, at a specific time.
+        /// </summary>
+        /// <typeparam name="TTask">The type of task to execute</typeparam>
+        /// <param name="days">The number of days to wait before executing the task again</param>
+        /// <param name="atHour">The hour fo the day to execute the task (0-23)</param>
+        /// <param name="atMinute">The minute of the hour to execute the task (0-60)</param>
+        /// <returns>Instance of IBackgroundTaskScheduler for method chaining</returns>
+        IBackgroundTaskScheduler RegisterAsyncRecurringTask<TTask>(int days, int atHour = 0, int atMinute = 0) where TTask : IAsyncRecurringBackgroundTask;
+
+        /// <summary>
+        /// Register a job to run every X number of hours.
+        /// </summary>
+        /// <typeparam name="TTask">The type of task to execute</typeparam>
+        /// <param name="hours">The number of hours to wait before executing the task again</param>
+        /// <param name="atMinute">The minute of the hour to execute the task (0-60)</param>
+        /// <returns>Instance of IBackgroundTaskScheduler for method chaining</returns>
+        IBackgroundTaskScheduler RegisterAsyncRecurringTask<TTask>(int hours = 0, int minute = 0) where TTask : IAsyncRecurringBackgroundTask;
+
+        /// <summary>
+        /// Register a job to run every X number of minutes.
+        /// </summary>
+        /// <typeparam name="TTask">The type of task to execute</typeparam>
+        /// <param name="minutes">The number of minutes to wait before executing the task again</param>
+        /// <returns>Instance of IBackgroundTaskScheduler for method chaining</returns>
+        IBackgroundTaskScheduler RegisterAsyncRecurringTask<TTask>(int minutes) where TTask : IAsyncRecurringBackgroundTask;
+
+        /// <summary>
+        /// Deregisters the specified job type (The class name/namespace is used as the identifier).
+        /// </summary>
+        /// <typeparam name="TTask">The type of task to execute</typeparam>
+        /// <returns>Instance of IBackgroundTaskScheduler for method chaining</returns>
+        IBackgroundTaskScheduler DeregisterAsyncRecurringTask<TTask>() where TTask : IAsyncRecurringBackgroundTask;
     }
 }
