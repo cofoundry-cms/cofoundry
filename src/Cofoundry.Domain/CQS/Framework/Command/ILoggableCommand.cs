90a291f56ebee4d71fd99b1df6878f459f7fc2ff
Joel Mitchell <me@joelmitchell.co.uk>
2017-03-06 11:11:34 +0000

    Fixes #74

--- src/Cofoundry.Domain/CQS/Framework/Command/ILoggableCommand.cs
+++ src/Cofoundry.Domain/CQS/Framework/Command/ILoggableCommand.cs
@@ -9,7 +9,7 @@ namespace Cofoundry.Domain.CQS
     /// <summary>
     /// Indicates that the execution of an ICommand should be logged.
     /// </summary>
-    public interface ILoggableCommand
+    public interface ILoggableCommand : ICommand
     {
     }
 }
