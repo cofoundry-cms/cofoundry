32edacd97d2dd6bbdee2de1efa5cb1676871c696
Joel Mitchell <me@joelmitchell.co.uk>
2017-06-01 11:22:38 +0100

    Fixed some issues with directories and document module scripts.

--- src/Cofoundry.Web.Admin/Admin/Modules/Auth/Content/js/ChangePassword_min.js
+++ src/Cofoundry.Web.Admin/Admin/Modules/Auth/Content/js/ChangePassword_min.js
@@ -1,2 +1,2 @@
-/*! UberCMS 2017-05-31 */
+/*! UberCMS 2017-06-01 */
 !function(a,b){a.addEventListener("DOMContentLoaded",function(){function c(){var c=a.getElementById("ReturnUrl"),d=b.location.hash;c.value&&d&&(c.value=c.value+d)}c()})}(document,window);
