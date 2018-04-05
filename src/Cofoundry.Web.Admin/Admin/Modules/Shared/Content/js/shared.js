4a706a3b2b6439185033e2ce4aceadafcc2689bb
Joel Mitchell <me@joelmitchell.co.uk>
2018-04-05 17:44:02 +0100

    HttpInterceptor no longer handles non-local failed requests

--- src/Cofoundry.Web.Admin/Admin/Modules/Shared/Content/js/shared.js
+++ src/Cofoundry.Web.Admin/Admin/Modules/Shared/Content/js/shared.js
@@ -5432,13 +5432,15 @@ angular.module('cms.shared').factory('httpInterceptor', [
     'shared.validationErrorService',
     'authenticationService',
     'shared.errorService',
+    'shared.stringUtilities',
 function (
     $q,
     $rootScope,
     _,
     validationErrorService,
     authenticationService,
-    errorService) {
+    errorService,
+    stringUtilities) {
 
     var service = {};
 
@@ -5455,39 +5457,47 @@ function (
     service.responseError = function (response) {
         var error;
 
-        switch (response.status) {
-            case 400:
-                /* Bad Request */
-                if (response.data.isValid === false) {
-                    validationErrorService.raise(response.data.errors);
-                }
-                break;
-            case 401:
-                /* Unauthorized */
-                authenticationService.redirectToLogin();
-                break;
-            case 403:
-                /* Forbidden (authenticated but not permitted to view resource */
-                errorService.raise({
-                    title: 'Permission Denied',
-                    message: 'This action is not authorized'
-                });
-                break;
-            default:
-                /* Allow get/404 responses through, otherwise raise an error. */
-                if (response.status != 404 || response.config.method !== 'GET') {
-                    error = {
-                        title: response.statusText,
-                        message: 'An unexpected server error has occured.',
-                        response: response
-                    };
-                    
-                    errorService.raise(error);
-                }
-                break;
+        // if not a local request, don't handle
+        if (stringUtilities.startsWith(response.config.url, '/')) {
+            handleLocalRequest();
         }
 
         return $q.reject(response);
+
+        function handleLocalRequest() {
+
+            switch (response.status) {
+                case 400:
+                    /* Bad Request */
+                    if (response.data.isValid === false) {
+                        validationErrorService.raise(response.data.errors);
+                    }
+                    break;
+                case 401:
+                    /* Unauthorized */
+                    authenticationService.redirectToLogin();
+                    break;
+                case 403:
+                    /* Forbidden (authenticated but not permitted to view resource */
+                    errorService.raise({
+                        title: 'Permission Denied',
+                        message: 'This action is not authorized'
+                    });
+                    break;
+                default:
+                    /* Allow get/404 responses through, otherwise raise an error. */
+                    if (response.status != 404 || response.config.method !== 'GET') {
+                        error = {
+                            title: response.statusText,
+                            message: 'An unexpected server error has occured.',
+                            response: response
+                        };
+
+                        errorService.raise(error);
+                    }
+                    break;
+            }
+        }
     };
 
     return service;
