ce1bbd8ab19692b2f7fe4acb4d16371a4eb01ecf
Joel Mitchell <me@joelmitchell.co.uk>
2018-06-28 14:43:04 +0100

    Updated docs on DbDateTimeMapper to detail why native EF core mappers aren't used.

--- src/Cofoundry.Domain/Domain/Shared/Mapping/DbDateTimeMapper.cs
+++ src/Cofoundry.Domain/Domain/Shared/Mapping/DbDateTimeMapper.cs
@@ -5,9 +5,14 @@ using System.Text;
 namespace Cofoundry.Domain
 {
     /// <summary>
-    /// Used to specify a datetime as DateTimeKind.UTC. To be used
-    /// until EF core supports specfying the DateTimeKind (supported in 2.1)
+    /// Used to specify a datetime as DateTimeKind.UTC.
     /// </summary>
+    /// <remarks>
+    /// This was intended to be replaced with EF type mapping which
+    /// is supported in EF Core 2.1, but there are some limitations with
+    /// query translations so it didn't seem worth the effort of making the change
+    /// until a clear need for it arises.
+    /// </remarks>
     public static class DbDateTimeMapper
     {
         public static DateTime AsUtc(DateTime source)
