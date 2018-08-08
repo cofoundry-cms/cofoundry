b7f418749314b9cd6772353c7f27231edb1a4d9d
Joel Mitchell <me@joelmitchell.co.uk>
2018-08-08 10:09:53 +0100

    Fixes #225 Increase the size of the CustomEntityModelType field in the PageTemplate table to account for longer namespaces

---
+++ src/Cofoundry.Domain/Install/Db/Schema/00007.sql
@@ -0,0 +1,7 @@
+﻿/* 
+	#225 Increase the size of the CustomEntityModelType field in the PageTemplate 
+	table to account for longer namespaces
+ */
+ alter table Cofoundry.PageTemplate alter column CustomEntityModelType nvarchar(400) null
+ go
+
