0818e5990c1a98a570f62fb480ab8884609b3007
Joel Mitchell <me@joelmitchell.co.uk>
2019-04-13 07:21:14 +0100

    Fixes #309 - Links in admin email templates do not account for a custom admin directory. Note that this is a temporary fix as this area is being reworked in the Cofoundry v0.7.

--- src/Cofoundry.Domain/MailTemplates/Constants/UrlLibrary.cs
+++ src/Cofoundry.Domain/MailTemplates/Constants/UrlLibrary.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Extensions.Configuration;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 
@@ -12,10 +13,42 @@ namespace Cofoundry.Domain.MailTemplates
 
         public static string PasswordReset(IResetPasswordTemplate template)
         {
+            return PasswordReset(template, ADMIN_ROOT);
+        }
+
+        private static string PasswordReset(IResetPasswordTemplate template, string basePath)
+        {
             return string.Format("{0}/auth/reset-password?i={1}&t={2}",
-                ADMIN_ROOT,
+                basePath,
                 template.UserPasswordResetRequestId.ToString().Replace("-", string.Empty),
                 Uri.EscapeDataString(template.Token));
+        }
+
+        /// <summary>
+        /// Temporary version to fix issue with non-standard admin path. This
+        /// will be refactored in the next update.
+        /// </summary>
+        public static string PasswordReset(IResetPasswordTemplate template, IConfiguration configuration)
+        {
+            return PasswordReset(template, GetAdminPath(configuration));
+        }
+
+        private static string GetAdminPath(IConfiguration configuration)
+        {
+            const string defaultPath = "admin";
+
+            var path = configuration.GetValue<string>("Cofoundry:Admin:DirectoryName");
+
+            return "~/" + (string.IsNullOrWhiteSpace(path) ? defaultPath : path);
+        }
+
+        /// <summary>
+        /// Temporary version to fix issue with non-standard admin path. This
+        /// will be refactored in the next update.
+        /// </summary>
+        public static string LoginUrl(IConfiguration configuration)
+        {
+            return GetAdminPath(configuration);
         }
     }
 }
