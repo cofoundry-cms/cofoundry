27cfdf47124847737b76cc1b1ed681ef3694a840
Joel Mitchell <2780344+HeyJoel@users.noreply.github.com>
2020-12-09 21:36:28 +0000

    * Updates to data annotation docs * Added tags to DocumentAssetAttribute constructor to put it inline with DocumentAttribute

--- src/Cofoundry.Domain/Domain/DocumentAssets/DataAnnotations/DocumentCollectionAttribute.cs
+++ src/Cofoundry.Domain/Domain/DocumentAssets/DataAnnotations/DocumentCollectionAttribute.cs
@@ -10,8 +10,8 @@ using Microsoft.AspNetCore.Mvc.ModelBinding.Metadata;
 namespace Cofoundry.Domain
 {
     /// <summary>
-    /// Use this to decorate an integer collection of DocumentAssetIds and indicate that it should be a collection 
-    /// of document assets. The editor allows for sorting of linked document assets and you can set filters for restricting file types.
+    /// This data annotation can be used to decorate a collection of integers, indicating the property represents a set 
+    /// of document asset ids. The editor allows for sorting and you can set filters for restricting file types.
     /// </summary>
     [AttributeUsage(AttributeTargets.Property)]
     public class DocumentCollectionAttribute : Attribute, IMetadataAttribute
@@ -21,6 +21,16 @@ namespace Cofoundry.Domain
         public DocumentCollectionAttribute()
             : base()
         {
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="DocumentCollectionAttribute"/> class.
+        /// </summary>
+        /// <param name="tags">An array of tags for which to filter when browsing for document.</param>
+        public DocumentCollectionAttribute(params string[] tags)
+            : base()
+        {
+            Tags = tags ?? new string[0];
         }
 
         #endregion
@@ -56,12 +66,14 @@ namespace Cofoundry.Domain
         #region properties
 
         /// <summary>
-        /// Restrict the input to allow only documents with these file extensions
+        /// Filters the document selection to only show documents with these 
+        /// file extensions.
         /// </summary>
         public string[] FileExtensions { get; set; }
 
         /// <summary>
-        /// Restrict the input to allow documents with only these tags
+        /// Filters the document selection to only show documents with tags that 
+        /// match this value.
         /// </summary>
         public string[] Tags { get; set; }
 
