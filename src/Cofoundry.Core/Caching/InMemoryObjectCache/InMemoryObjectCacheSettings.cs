4509d080b64855f61dd153fd34776b7f5996b28d
Joel Mitchell <jmail.mitchell@elvislondon.com>
2020-12-15 17:47:22 +0000

    Updated xml doc

--- src/Cofoundry.Core/Caching/InMemoryObjectCache/InMemoryObjectCacheSettings.cs
+++ src/Cofoundry.Core/Caching/InMemoryObjectCache/InMemoryObjectCacheSettings.cs
@@ -13,7 +13,9 @@ namespace Cofoundry.Core.Caching
         /// <summary>
         /// The cache mode that should be used to determine the lifetime
         /// of data stored in the cache. Defaults to InMemoryObjectCacheMode.Persitent,
-        /// which is the preferred mode for a single-server deployment.
+        /// which is the preferred mode for a single-server deployment. 
+        /// InMemoryObjectCacheMode.PerScope can be used to enable a simple multi-server
+        /// deployment.
         /// </summary>
         public InMemoryObjectCacheMode CacheMode { get; set; } = InMemoryObjectCacheMode.Persitent;
     }
